238a05c8dfcd7c0ee58c07991586628255749f34
Wevan <438864121@qq.com>
2018-12-16 19:29:41 +0800

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
-  title = 'ttt';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'https://angular.io/guide/styleguide', 3),
+      new Article('FullStack', 'https://semantic-ui.com/elements/button.html', 10),
+      new Article('Homepage', 'https://github.com/ng-book2/code/', 6)
+    ];
+  }
+
+  sortArticles(): Article[] {
+    return this.articles.sort(((a, b) => b.votes - a.votes));
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title:${title.value} and link:${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent,
+    UserListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
